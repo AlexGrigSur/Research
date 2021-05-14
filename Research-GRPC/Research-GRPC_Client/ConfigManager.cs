575279c87947c5a0545b474b1c70e3ef4c7ed80b
AlexGrigSur <alex.grig.sur@gmail.com>
2021-05-14 12:04:04 +0300

    base client-server grpc

---
+++ Research-GRPC/Research-GRPC_Client/ConfigManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Research_GRPC_Client
+{
+    public static class ConfigManager
+    {
+        public static string GetField(string fieldName)
+        {
+            // TODO: Get fields from appsettings.json
+
+            return "https://localhost:5001";
+        }
+    }
+}
---
+++ Research-GRPC/Research-GRPC_Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Net.Client;
+using Research_GRPC;
+
+namespace Research_GRPC_Client
+{
+    // https://www.youtube.com/watch?v=QyxCX2GYHxk&ab_channel=IAmTimCorey
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // APPSETTINGS.JSON
+            string connectionData = ConfigManager.GetField("ConnectionStrings");
+            var channel = GrpcChannel.ForAddress(connectionData);
+
+            HelloRequest request = new HelloRequest(){Name = "Alex"};
+
+            var client = new Greeter.GreeterClient(channel);
+
+            SendMessage.Send(client,request).Wait();
+            Console.ReadKey();
+        }
+    }
+}
