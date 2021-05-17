ea9eb746ca572447bea9efe1dfa34052e6579592
AlexGrigSur <alex.grig.sur@gmail.com>
2021-05-17 08:11:52 +0300

    Client-Server interaction(part 2)

--- Research-GRPC/Research-GRPC_Client/Program.cs
+++ Research-GRPC/Research-GRPC_Client/Program.cs
@@ -9,31 +9,12 @@ namespace Research_GRPC_Client
     {
         static void Main(string[] args)
         {
-            // APPSETTINGS.JSON
-
-
             // make static configManager?
             var config = new ConfigManager().GetConfiguration();
 
             string connectionData = config["HostURL"];
 
-            var channel = GrpcChannel.ForAddress(connectionData);
-
-            UserModel model = new UserModel()
-            {
-                FirstName = "Hello",
-                LastName = "it",
-                Email = "is",
-                Password = "me"
-            };
-
-            var client = new UsersHandler.UsersHandlerClient(channel);
-
-            var replyID = client.SetRequest(model);
-            Console.WriteLine("UserID:{0}",replyID.Id);
-
-            var replyModel = client.GetRequest(replyID);
-            Console.WriteLine("FirstName:{0}", replyModel.FirstName);
+            new UsersHandlerTest().BeginTest(connectionData);
 
             Console.ReadKey();
         }
---
+++ Research-GRPC/Research-GRPC_Client/UsersHandlerTest.cs
@@ -0,0 +1,88 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Research_GRPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Research_GRPC_Client
+{
+    public class UsersHandlerTest
+    {
+        public void BeginTest(string connectionString)
+        {
+            var channel = GrpcChannel.ForAddress(connectionString);
+            var client = new UsersHandler.UsersHandlerClient(channel);
+
+            GetRequest(client, 5);
+            Console.WriteLine("*************");
+            GetRequest(client, 1);
+            Console.WriteLine("*************");
+            SetRequest(client);
+            Console.WriteLine("*************");
+            GetAllUsers(client);
+        }
+
+        private void GetRequest(UsersHandler.UsersHandlerClient client, int id)
+        {
+            UserID userID = new UserID()
+            {
+                Id = id
+            };
+            try
+            {
+                var reply = client.GetRequest(userID);
+                Console.WriteLine($"GetRequest result: FirstName - {reply.FirstName}");
+            }
+            catch (Grpc.Core.RpcException)
+            {
+                Console.WriteLine("Result is null");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled exception");
+            }
+        }
+        private async void GetAllUsers(UsersHandler.UsersHandlerClient client)
+        {
+            Console.WriteLine("GetAllUsers request");
+            try
+            {
+                using (var call = client.GetAllUsers(new Empty()))
+                {
+                    while (await call.ResponseStream.MoveNext())
+                    {
+                        var currentReply = call.ResponseStream.Current;
+                        Console.WriteLine($"GetAllUsers reply: FirstName - {currentReply.FirstName}");
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Unhandled exception");
+            }
+        }
+        private void SetRequest(UsersHandler.UsersHandlerClient client)
+        {
+            UserModel model = new UserModel()
+            {
+                FirstName = "Hello",
+                LastName = "it",
+                Email = "is",
+                Password = "me"
+            };
+
+            try
+            {
+                var reply = client.SetRequest(model);
+                Console.WriteLine($"SetRequest result: {reply.Id}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled exception");
+            }
+        }
+    }
+}
